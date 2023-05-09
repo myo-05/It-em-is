50d38aec2fe451fd37eacaa34c699b7809103152
Young-Oh <gzf0547@gmail.com>
2023-05-09 15:21:39 +0900

    add postings app & modify settings timezone laguage

---
+++ postings/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from postings import views
+
+urlpatterns = [
+    path('', views.PostingView.as_view(), name='posting_list'),
+    path('<str:id>/', views.PostingDetail.as_view(), name='posting_detail')
+]
--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,7 @@ from rest_framework_simplejwt.views import (
 from users import views
 
 urlpatterns = [
-    path('api/token/', views.CustomTokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
+    path('api/token/', views.CustomTokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('signup/', views.Userview.as_view()),
     path('mock/', views.mockview.as_view())
