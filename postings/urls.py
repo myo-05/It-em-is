83f0ecd9872dad5fdfb996392763b56941acf6d0
Young-Oh <gzf0547@gmail.com>
2023-05-09 15:24:30 +0900

    create feature/postings branch

--- postings/urls.py
+++ postings/urls.py
@@ -3,5 +3,5 @@ from postings import views
 
 urlpatterns = [
     path('', views.PostingView.as_view(), name='posting_list'),
-    path('<str:id>/', views.PostingDetail.as_view(), name='posting_detail')
+    path('<str:id>/', views.PostingDetail.as_view(), name='posting_detail'),
 ]
