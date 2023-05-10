164384c51f2eb8b99885d7c3a7ca6817e54b5464
Young-Oh <gzf0547@gmail.com>
2023-05-10 12:04:25 +0900

    주석제거

--- prac/settings.py
+++ prac/settings.py
@@ -80,13 +80,6 @@ WSGI_APPLICATION = 'prac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('ENGINE'),
